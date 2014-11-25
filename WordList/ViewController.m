5e541d8d1a021c4e410309d2db811450f2d3b0e7
hpsoar <hpsoar@gmail.com>
2014-11-25 13:27:25 +0800

    ..

--- WordList/ViewController.m
+++ WordList/ViewController.m
@@ -7,21 +7,57 @@
 //
 
 #import "ViewController.h"
+#import "AFHTTPSessionManager.h"
+
+NSString* const kYoudaoKeyFrom  = @"kernelpanic";
+NSString* const kYoudaokey      = @"482091942";
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(lookupWordInPasteBoard) name:UIApplicationWillEnterForegroundNotification object:nil];
+    
+    self.label.numberOfLines = 0;
+    [self lookupWordInPasteBoard];
+}
+
+- (void)lookupWordInPasteBoard {
+    NSString *word = [UIPasteboard generalPasteboard].string;
+    NSLog(@"%@", word);
+    if (word) {
+        [self queryYoudao:word];
+    }
+}
+
+-(void)queryYoudao:(NSString*)word {
+    NSString *const kYoudaoURL = @"http://fanyi.youdao.com";
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:kYoudaoURL]];
+    NSDictionary *params = @{ @"keyfrom": kYoudaoKeyFrom,
+                              @"key": kYoudaokey,
+                              @"type": @"data",
+                              @"doctype": @"json",
+                              @"version": @1.1,
+                              @"q": word };
+    [manager GET:@"openapi.do" parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSLog(@"%@", responseObject);
+        NSArray *translations = responseObject[@"translation"];
+        self.label.text = [translations componentsJoinedByString:@"\n"];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"%@", error);
+    }];
 }
 
 @end
